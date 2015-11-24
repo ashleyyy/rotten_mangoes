c4b9471e43b98f22305238aa8024682129d90b21
ashleyyy <ashleyfisher@gmail.com>
2015-11-24 03:27:47 +0000

    added links to log in, sign up, or log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
-
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
